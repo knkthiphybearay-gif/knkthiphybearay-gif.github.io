7adf151b37d19e5574c1f739ad1dd0735c9ce644
knkthiphybearay-gif <knkthiphybearay@gmail.com>
2026-09-01 09:09:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded',function () {
+   document.getElementById('year').innerHTML = new Date().getFullYear();
+   console.log('Portfolio ของ fairly พร้อมแล้ว!');
+});
